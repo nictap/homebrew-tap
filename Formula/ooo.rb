090038a52c72aa9210d3ddcafcf5a81617c2490b
nictap <154410159+nictap@users.noreply.github.com>
2023-12-20 09:10:55 +0100

    Update ooo.rb with new shasum
    Previous release was faulty.

--- Formula/ooo.rb
+++ Formula/ooo.rb
@@ -2,7 +2,7 @@ class Ooo < Formula
   desc "CLI for piping outputs to ollama or just prompting"
   homepage "https://github.com/Npahlfer/ooo"
   url "https://github.com/Npahlfer/ooo/releases/download/0.1.0/ooo-0.1.0-arm64-apple-darwin.tar.gz"
-  sha256 "a4239840e6e6e9a069470b36e73e4525ba6bbae37bf2f38ab1db9fd906d1591c"
+  sha256 "964adb3cfd9480fe43838634d52299859011fcd3945ca101fd31601da3529b25"
   version "0.1.0"
 
   def install

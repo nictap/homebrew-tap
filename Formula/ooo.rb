679ab7f590901551cf34f41e7bd3ab3fcc5003a0
nictap <154410159+nictap@users.noreply.github.com>
2023-12-27 11:10:27 +0100

    Update ooo.rb

--- Formula/ooo.rb
+++ Formula/ooo.rb
@@ -1,8 +1,8 @@
 class Ooo < Formula
   desc "CLI for piping outputs to ollama or just prompting"
   homepage "https://github.com/Npahlfer/ooo"
-  url "https://github.com/Npahlfer/ooo/releases/download/0.1.0/ooo-0.1.0-arm64-apple-darwin.tar.gz"
-  sha256 "964adb3cfd9480fe43838634d52299859011fcd3945ca101fd31601da3529b25"
+  url "https://github.com/Npahlfer/ooo/releases/download/0.1.1/ooo-0.1.1-arm64-apple-darwin.tar.gz"
+  sha256 "4918e0362fb676ecdaabb42ddf5f0daca56393d24e94043b368eb93442effe48"
   version "0.1.0"
 
   def install

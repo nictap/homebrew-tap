4a749fc24c82b405938521420b623ca6507a71f1
nictap <154410159+nictap@users.noreply.github.com>
2024-07-18 09:27:33 +0200

    Removes redundant version

--- Formula/ooo.rb
+++ Formula/ooo.rb
@@ -3,7 +3,6 @@ class Ooo < Formula
   homepage "https://github.com/Npahlfer/ooo"
   url "https://github.com/Npahlfer/ooo/releases/download/0.1.2/ooo-0.1.2-arm64-apple-darwin.tar.gz"
   sha256 "d145ff438aa7b85ec502e7a1d05ece28fe5c61d8fb4fa67ce89e2a467237bbdc"
-  version "0.1.2"
 
   def install
     bin.install "ooo"

ba587a040b1566564b721bf31dfba18e3d0dd940
nictap <154410159+nictap@users.noreply.github.com>
2024-07-18 09:15:48 +0200

    Changes url

--- Formula/ooo.rb
+++ Formula/ooo.rb
@@ -1,7 +1,7 @@
 class Ooo < Formula
   desc "CLI for piping outputs to ollama or just prompting"
   homepage "https://github.com/Npahlfer/ooo"
-  url "https://github.com/Npahlfer/ooo/releases/download/0.1.1/ooo-0.1.2-arm64-apple-darwin.tar.gz"
+  url "https://github.com/Npahlfer/ooo/releases/download/0.1.2/ooo-0.1.2-arm64-apple-darwin.tar.gz"
   sha256 "d145ff438aa7b85ec502e7a1d05ece28fe5c61d8fb4fa67ce89e2a467237bbdc"
   version "0.1.2"
 
